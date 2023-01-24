289b4cd125cd04a1fff65694bd032a9e122bd2df
JenyaDoombrovsky <crudtechcompany@gmail.com>
2023-01-24 12:27:28 +0100

    added first part of privacy policy page

--- src/components/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,11 +1,173 @@
 import style from "./PrivacyPolicy.module.css";
 
 const PrivacyPolicy = () => {
-    return (
-        <div className={style["block"]}>
-
-        </div>
-    );
-}
+  return (
+    <div className={style["block"]}>
+      <div className={style["content"]}>
+        <h2>Polityka Prywatności</h2>
+        <p>
+          Poniższa Polityka Prywatności określa 
+          <b>
+            zasady zapisywania i uzyskiwania dostępu do danych na Urządzeniach
+            Użytkowników
+          </b>{" "}
+          korzystających z Serwisu do celów świadczenia usług drogą
+          elektroniczną przez Administratora oraz 
+          <b>
+            zasady gromadzenia i przetwarzania danych osobowych Użytkowników
+          </b>
+          , które zostały podane przez nich osobiście i dobrowolnie za
+          pośrednictwem narzędzi dostępnych w Serwisie.
+        </p>
+        <p>
+          Poniższa Polityka Prywatności jest integralną częścią Regulaminu
+          Serwisu, który określa zasady, prawa i obowiązki Użytkowników
+          korzystających z Serwisu.
+        </p>
+        <h3>§1 Definicje</h3>
+        <ul>
+          <li>
+            <b>Serwis</b> - serwis internetowy "Finance Expert" działający pod
+            adresem finexp.pl
+          </li>
+          <li>
+            <b>Serwis zewnętrzny</b> - serwisy internetowe partnerów,
+            usługodawców lub usługobiorców współpracujących z Administratorem
+          </li>
+          <li>
+            <b>Administrator Serwisu / Danych</b> - Administratorem Serwisu oraz
+            Administratorem Danych (dalej Administrator) jest firma Finance
+            Expert Sp. z o.o., prowadząca działalność pod adresem: ul. Krupnicza
+            2-4/31, 50-074 Wroclaw, o nadanym numerze identyfikacji podatkowej
+            (NIP): 8971906842, świadcząca usługi drogą elektroniczną za
+            pośrednictwem Serwisu
+          </li>
+          <li>
+            <b>Użytkownik </b>- osoba fizyczna, dla której Administrator
+            świadczy usługi drogą elektroniczną za pośrednictwem Serwisu.
+          </li>
+          <li>
+            <b>Urządzenie </b>- elektroniczne urządzenie wraz z oprogramowaniem,
+            za pośrednictwem którego Użytkownik uzyskuje dostęp do Serwisu
+          </li>
+          <li>
+            <b>Cookies (ciasteczka)</b> - dane tekstowe gromadzone w formie
+            plików zamieszczanych na Urządzeniu Użytkownika
+          </li>
+          <li>
+            <b>RODO </b>- Rozporządzenie Parlamentu Europejskiego i Rady (UE)
+            2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób
+            fizycznych w związku z przetwarzaniem danych osobowych i w sprawie
+            swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE
+            (ogólne rozporządzenie o ochronie danych)
+          </li>
+          <li>
+            <b>Dane osobowe</b> - oznaczają informacje o zidentyfikowanej lub
+            możliwej do zidentyfikowania osobie fizycznej („osobie, której dane
+            dotyczą”); możliwa do zidentyfikowania osoba fizyczna to osoba,
+            którą można bezpośrednio lub pośrednio zidentyfikować, w
+            szczególności na podstawie identyfikatora takiego jak imię i
+            nazwisko, numer identyfikacyjny, dane o lokalizacji, identyfikator
+            internetowy lub jeden bądź kilka szczególnych czynników
+            określających fizyczną, fizjologiczną, genetyczną, psychiczną,
+            ekonomiczną, kulturową lub społeczną tożsamość osoby fizycznej
+          </li>
+          <li>
+            <b>Przetwarzanie </b>- oznacza operację lub zestaw operacji
+            wykonywanych na danych osobowych lub zestawach danych osobowych w
+            sposób zautomatyzowany lub niezautomatyzowany, taką jak zbieranie,
+            utrwalanie, organizowanie, porządkowanie, przechowywanie,
+            adaptowanie lub modyfikowanie, pobieranie, przeglądanie,
+            wykorzystywanie, ujawnianie poprzez przesłanie, rozpowszechnianie
+            lub innego rodzaju udostępnianie, dopasowywanie lub łączenie,
+            ograniczanie, usuwanie lub niszczenie;
+          </li>
+          <li>
+            <b>Ograniczenie przetwarzania</b> - oznacza oznaczenie
+            przechowywanych danych osobowych w celu ograniczenia ich przyszłego
+            przetwarzania
+          </li>
+          <li>
+            <b>Profilowanie </b>- oznacza dowolną formę zautomatyzowanego
+            przetwarzania danych osobowych, które polega na wykorzystaniu danych
+            osobowych do oceny niektórych czynników osobowych osoby fizycznej, w
+            szczególności do analizy lub prognozy aspektów dotyczących efektów
+            pracy tej osoby fizycznej, jej sytuacji ekonomicznej, zdrowia,
+            osobistych preferencji, zainteresowań, wiarygodności, zachowania,
+            lokalizacji lub przemieszczania się
+          </li>
+          <li>
+            <b>Zgoda </b>- zgoda osoby, której dane dotyczą oznacza dobrowolne,
+            konkretne, świadome i jednoznaczne okazanie woli, którym osoba,
+            której dane dotyczą, w formie oświadczenia lub wyraźnego działania
+            potwierdzającego, przyzwala na przetwarzanie dotyczących jej danych
+            osobowych
+          </li>
+          <li>
+            <b>Naruszenie ochrony danych osobowych</b> - oznacza naruszenie
+            bezpieczeństwa prowadzące do przypadkowego lub niezgodnego z prawem
+            zniszczenia, utracenia, zmodyfikowania, nieuprawnionego ujawnienia
+            lub nieuprawnionego dostępu do danych osobowych przesyłanych,
+            przechowywanych lub w inny sposób przetwarzanych
+          </li>
+          <li>
+            <b>Pseudonimizacja </b>- oznacza przetworzenie danych osobowych w
+            taki sposób, by nie można ich było już przypisać konkretnej osobie,
+            której dane dotyczą, bez użycia dodatkowych informacji, pod
+            warunkiem że takie dodatkowe informacje są przechowywane osobno i są
+            objęte środkami technicznymi i organizacyjnymi uniemożliwiającymi
+            ich przypisanie zidentyfikowanej lub możliwej do zidentyfikowania
+            osobie fizycznej
+          </li>
+          <li>
+            <b>Anonimizacja </b>- Anonimizacja danych to nieodwracalny proces
+            operacji na danych, który niszczy / nadpisuje "dane osobowe"
+            uniemożliwiając identyfikację, lub powiązanie danego rekordu z
+            konkretnym użytkownikiem lub osobą fizyczną.
+          </li>
+        </ul>
+        <h3>§2 Inspektor Ochrony Danych</h3>
+        <p>
+          Na podstawie Art. 37 RODO, Administrator nie powołał Inspektora
+          Ochrony Danych.
+        </p>
+        <p>
+          W sprawach dotyczących przetwarzania danych, w tym danych osobowych,
+          należy kontaktować się bezpośrednio z Administratorem.
+        </p>
+        <h3>§3 Rodzaje Plików Cookies</h3>
+        <ul>
+          <li>
+            <b>Cookies wewnętrzne</b> - pliki zamieszczane i odczytywane z
+            Urządzenia Użytkownika przez system teleinformatyczny Serwisu
+          </li>
+          <li>
+            <b>Cookies zewnętrzne</b> - pliki zamieszczane i odczytywane z
+            Urządzenia Użytkownika przez systemy teleinformatyczne Serwisów
+            zewnętrznych. Skrypty Serwisów zewnętrznych, które mogą umieszczać
+            pliki Cookies na Urządzeniach Użytkownika zostały świadomie
+            umieszczone w Serwisie poprzez skrypty i usługi udostępnione i
+            zainstalowane w Serwisie
+          </li>
+          <li>
+            <b>Cookies sesyjne</b> - pliki zamieszczane i odczytywane z
+            Urządzenia Użytkownika przez Serwis podczas jednej sesji danego
+            Urządzenia. Po zakończeniu sesji pliki są usuwane z Urządzenia
+            Użytkownika.
+          </li>
+          <li>
+            <b>Cookies trwałe</b> - pliki zamieszczane i odczytywane z
+            Urządzenia Użytkownika przez Serwis do momentu ich ręcznego
+            usunięcia. Pliki nie są usuwane automatycznie po zakończeniu sesji
+            Urządzenia chyba że konfiguracja Urządzenia Użytkownika jest
+            ustawiona na tryb usuwanie plików Cookie po zakończeniu sesji
+            Urządzenia.
+          </li>
+        </ul>
+        <h3>§4 Bezpieczeństwo składowania danych</h3>
+      </div>
+    </div>
+  );
+};
 
 export default PrivacyPolicy;
